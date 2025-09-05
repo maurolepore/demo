5af810182207444fdeb86eaccd392c874bf6483a
Mauro Lepore <maurolepore@gmail.com>
2025-09-05 16:35:44 -0700

    Update
    [ghstack-poisoned]

--- tests/testthat/test-hi.R
+++ tests/testthat/test-hi.R
@@ -5,3 +5,11 @@ test_that("defaults to greeting the world", {
 test_that("greets me", {
   expect_equal(hi("me"), "hello me")
 })
+
+test_that("greets you", {
+  expect_equal(hi("you"), "hello you")
+})
+
+test_that("bye works", {
+  expect_equal(bye(), "bye")
+})
